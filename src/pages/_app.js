13fcf461573cc689ad3d022ac6312855e4e64d62
msagres <m_costa3@etu-webschoolfactory.fr>
2023-03-24 11:16:08 +0100

    feat: nextui added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,13 @@
 import '@/styles/globals.css';
 import {ThirdwebProvider} from '@thirdweb-dev/react';
+import {NextUIProvider} from '@nextui-org/react';
 
 export default function App({Component, pageProps}) {
     return (
-        <ThirdwebProvider activeChain="mumbai">
-            <Component {...pageProps} />
-        </ThirdwebProvider>
+        <NextUIProvider>
+            <ThirdwebProvider activeChain="mumbai">
+                <Component {...pageProps} />
+            </ThirdwebProvider>
+        </NextUIProvider>
     );
 }
